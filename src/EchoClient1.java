34cd0791963b8a6421ca4b013bf935da86544e33
James D. Mc Garr <t00126681@365s.ittralee.ie>
2016-10-25 12:59:26 +0100

    small changes

--- src/EchoClient1.java
+++ src/EchoClient1.java
@@ -2,6 +2,7 @@
  * Created by t00126681 on 25/10/2016.
  */
 import java.io.*;
+import java.util.Scanner;
 
 /**
  * This module contains the presentaton logic of an Echo Client.
@@ -10,35 +11,28 @@ import java.io.*;
 public class EchoClient1 {
     static final String endMessage = ".";
     public static void main(String[] args) {
-        InputStreamReader is = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(is);
+        Scanner input = new Scanner(System.in);
         try {
-            System.out.println("Welcome to the Echo client.\n" +
-                    "What is the name of the server host?");
-            String hostName = br.readLine();
-            if (hostName.length() == 0) // if user did not enter a name
-                hostName = "localhost";  //   use the default host name
-            System.out.println("What is the port number of the server host?");
-            String portNum = br.readLine();
-            if (portNum.length() == 0)
-                portNum = "7";          // default port number
-            EchoClientHelper1 helper =
-                    new EchoClientHelper1(hostName, portNum);
-            boolean done = false;
-            String message, echo;
-            while (!done) {
-                System.out.println("Enter a line to receive an echo back from the server, "
-                        + "or a single peroid to quit.");
-                message = br.readLine( );
-                if ((message.trim()).equals (endMessage)){
-                    done = true;
-                    helper.done( );
-                }
-                else {
-                    echo = helper.getEcho( message);
-                    System.out.println(echo);
-                }
-            } // end while
+            String hostName = "localhost";
+            String portNum = "9601";
+
+            System.out.println("Login.\nEnter username: ");
+            String username = input.nextLine().trim();
+
+            System.out.println("What is your password: ");
+            String password = input.nextLine().trim();
+
+            EchoClientHelper1 helper = new EchoClientHelper1(hostName, portNum);
+
+
+
+            String echo;
+            echo = helper.getEcho("100:username:" + username + ":password:" + password);
+
+            helper.done();
+            System.out.println(echo);
+
+
         } // end try
         catch (Exception ex) {
             ex.printStackTrace( );
